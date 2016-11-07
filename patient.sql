8fb9ae9f657829720b5dd6d9760bcdc64cd1d56a
Seth Wulbecker <Seth@swulbs-macbook.student.wheaton.edu>
2016-11-07 15:22:49 -0600

    adding patient

---
+++ patient.sql
@@ -0,0 +1,15 @@
+DROP TABLE patient;
+CREATE TABLE patient (
+  pssn        char(9) not null,
+  fname       varchar(15),
+  minit		  char(1),
+  lname		  varchar(15),
+  inits       char(3),
+  address	  varchar(45),
+  sex		  char(1),
+  age		  int,
+  date_admit  date, 
+  mgrstartdate date,
+  primary key (dnumber),
+  key (dname)
+);
