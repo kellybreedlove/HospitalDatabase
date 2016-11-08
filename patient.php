d65c00afc22b2c8aeee03d0a8761e522845885b1
kelly breedlove <kellybreedlove5@gmail.com>
2016-11-08 15:33:41 -0600

    made patient.php

---
+++ patient.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Patient Search</title>
+	</head>
+
+<body>
+
+<?php
+  $host="localhost";
+  $user="root";
+  $pw="root";
+  $db="hospital";
+
+  $con = new mysqli ($host, $user, $pw, $db);
+
+  $query="SELECT pssn FROM patient";
+  $result=$con->query($query);
+  $rows=$result->num_rows;
+
+  echo "There are $rows patients.";
+
+?>
+
+<h4>Patient Details for: </h4>
+<form method="post" action="patientView.php">
+	<select name="ssn">
+
+		<?php
+  
+		while ($pssn = $result->fetch_assoc()) {
+			echo "<option>", $pssn['pssn'];
+		}
+		?>
+
+	</select>
+
+	<input type="submit" value="Get Patient Details">
+</form>
+
+</body>
+</html>
