9b2748b170f354f130cbb8da0df348530d1ed3b9
Seth Wulbecker <Seth@swulbs-macbook.student.wheaton.edu>
2016-11-18 12:35:23 -0600

    Updated missing fields

--- addPatient.php
+++ addPatient.php
@@ -13,16 +13,19 @@
  $db="hospital";
 
  $con = new mysqli ($host, $user, $pw, $db);
- 
+  
+ $ssn = $con->real_escape_string($_POST["ssn"])
  $fname = $con->real_escape_string($_POST["fname"])
  $minit = $con->real_escape_string($_POST["minit"])
  $lname = $con->real_escape_string($_POST["lname"])
- 
- $ssn = $con->real_escape_string($_POST["ssn"])
- 
  $address = $con->real_escape_string($_POST["address"])
- $age = $con->real_escape_string($_POST["age"])
  $sex = $con->real_escape_string($_POST["sex"])
+ $age = $con->real_escape_string($_POST["age"])
+ $date_admit = $con->real_escape_string($_POST["date_admit"])
+ $date_disch = $con->real_escape_string($_POST["date_disch"])
+ $contactno = $con->real_escape_string($_POST["contactno"])
+ $info = $con->real_escape_string($_POST["info"])
+ $rid = $con->real_escape_string($_POST["rid"])
 
  $query = "SELECT * FROM patient where ssn=$ssn";
  $result = $con->query($query);
@@ -32,8 +35,10 @@
   echo "The ssn exists in the patient table.";
  }
  else {
-  $query = "INSERT INTO patient (pssn, fname, minit, lname, address, sex, age) 
-  VALUES('$ssn', '$fname', '$minit', '$lname', '$address', '$sex', '$age')";
+  $query = "INSERT INTO patient (pssn, fname, minit, lname, address, sex, age, 
+  date_admit, date_disch, contactNum, info, rid) 
+  VALUES('$ssn', '$fname', '$minit', '$lname', '$address', '$sex', '$age', 
+  '$date_admit', '$date_disch', '$contactno', '$info', '$rid')";
   $result = $con->query($query);
   
   if ($result) {echo "Data successfully added.";}
