5f1b105a123e09a435922740258ead9016988415
Seth Wulbecker <Seth@swulbs-macbook.local>
2016-11-18 19:51:48 -0600

    adding a patient now works

--- addPatient.php
+++ addPatient.php
@@ -14,20 +14,20 @@
 
  $con = new mysqli ($host, $user, $pw, $db);
   
- $ssn = $con->real_escape_string($_POST["ssn"])
- $fname = $con->real_escape_string($_POST["fname"])
- $minit = $con->real_escape_string($_POST["minit"])
- $lname = $con->real_escape_string($_POST["lname"])
- $address = $con->real_escape_string($_POST["address"])
- $sex = $con->real_escape_string($_POST["sex"])
- $age = $con->real_escape_string($_POST["age"])
- $date_admit = $con->real_escape_string($_POST["date_admit"])
- $date_disch = $con->real_escape_string($_POST["date_disch"])
- $contactno = $con->real_escape_string($_POST["contactno"])
- $info = $con->real_escape_string($_POST["info"])
- $rid = $con->real_escape_string($_POST["rid"])
+ $ssn = $con->real_escape_string($_POST["ssn"]);
+ $fname = $con->real_escape_string($_POST["fname"]);
+ $minit = $con->real_escape_string($_POST["minit"]);
+ $lname = $con->real_escape_string($_POST["lname"]);
+ $address = $con->real_escape_string($_POST["address"]);
+ $sex = $con->real_escape_string($_POST["sex"]);
+ $age = $con->real_escape_string($_POST["age"]);
+ $date_admit = $con->real_escape_string($_POST["date_admit"]);
+ $date_disch = $con->real_escape_string($_POST["date_disch"]);
+ $contactno = $con->real_escape_string($_POST["contactno"]);
+ $info = $con->real_escape_string($_POST["info"]);
+ $rid = $con->real_escape_string($_POST["rid"]);
 
- $query = "SELECT * FROM patient where ssn=$ssn";
+ $query = "SELECT * FROM patient where pssn=$ssn";
  $result = $con->query($query);
  $rows=$result->num_rows;
  
@@ -35,10 +35,11 @@
   echo "The ssn exists in the patient table.";
  }
  else {
-  $query = "INSERT INTO patient (pssn, fname, minit, lname, address, sex, age, 
+  $query = "INSERT INTO patient(pssn, fname, minit, lname, address, sex, age, 
   date_admit, date_disch, contactNum, info, rid) 
   VALUES('$ssn', '$fname', '$minit', '$lname', '$address', '$sex', '$age', 
   '$date_admit', '$date_disch', '$contactno', '$info', '$rid')";
+  
   $result = $con->query($query);
   
   if ($result) {echo "Data successfully added.";}
