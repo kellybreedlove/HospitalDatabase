a09683787a0c4166f27f39f26e14bf22dbd5f71e
Seth Wulbecker <Seth@swulbs-macbook.student.wheaton.edu>
2016-12-10 11:09:40 -0600

    Updates

---
+++ docSched.php
@@ -0,0 +1,41 @@
+<!DOCTYPE>
+<html>
+<head>
+<title>Doctor Schedule</title>
+</head>
+<body>
+
+<?php
+ $host="localhost";
+ $user="root";
+ $pw="root";
+ $db="hospital";
+
+ $con = new mysqli ($host, $user, $pw, $db);
+  
+  $query="SELECT essn FROM employee WHERE doctor_flg = 'Y'";
+  $result=$con->query($query);
+  $rows=$result->num_rows;
+
+  echo "There are $rows doctors.";
+
+?>
+
+<h4>Doctor Schedule for: </h4>
+<form method="post" action="docSchedView.php">
+	<select name="ssn">
+
+		<?php
+  
+		while ($essn = $result->fetch_assoc()) {
+			echo "<option>", $essn['essn'];
+		}
+		?>
+
+	</select>
+
+	<input type="submit" value="Get Doctor's schedule">
+</form>
+
+</body>
+</html>
