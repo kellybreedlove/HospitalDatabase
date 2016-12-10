cb78f37850ea5438b016ae56d6509df2b04fede9
Seth Wulbecker <Seth@swulbs-macbook.local>
2016-12-10 09:29:34 -0600

    updates

--- create_tables/appointment.sql
+++ create_tables/appointment.sql
@@ -3,7 +3,7 @@ CREATE TABLE appointment (
   aid        char(3) not null,
   pssn       char(9),
   essn	     char(9),
-  rid		 char(3),
+  rid		 char(9),
   primary key (aid),
   foreign key (pssn) references patient(pssn),
   foreign key (essn) references employee(essn),
